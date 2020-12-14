590e8436b3fe14c797fe3496f2b318dd3ac53f96
Diego Obando <diegoobando20@gmail.com>
2020-12-14 01:55:07 -0300

    added functionality & styles to ScheduleDetailDialogFragment+ some datails

--- app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/platzi/conf/view/ui/activities/MainActivity.kt
@@ -2,12 +2,20 @@ package com.platzi.conf.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.platzi.conf.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.toolbarMain))
+        configNav()
+    }
 
+    fun configNav() {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
     }
 }
--- app/src/main/java/com/platzi/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/ScheduleDetailDialogFragment.kt
@@ -1,60 +1,60 @@
 package com.platzi.conf.view.ui.fragments
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.platzi.conf.R
+import com.platzi.conf.model.Conference
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import java.text.SimpleDateFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ScheduleDetailDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ScheduleDetailDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ScheduleDetailDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_schedule_detail_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ScheduleDetailDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ScheduleDetailDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Fragment configurations
+        toolbarConference.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarConference.setTitleTextColor(Color.WHITE)
+        toolbarConference.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val conference = arguments?.getSerializable("conference") as Conference
+        toolbarConference.title = conference.title
+
+        tvItemScheduleTituloConferencia.text = conference.title
+
+        val pattern = "dd/MM/yyyy hh:mm a"
+        val simpleDF = SimpleDateFormat(pattern)
+        val date = simpleDF.format(conference.dateTime)
+        tvDetailConferenceHour.text = date
+
+        tvDetailConferenceSpeaker.text = conference.speaker
+        tvDetailConferenceTag.text = conference.tag
+        tvDetailConferenceDescription.text = conference.description
+    }
+
+    // Set the screen size
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
