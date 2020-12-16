a6aa4db5838c509b64efd9f720c3a0110e3cec99
Diego Obando <diegoobando20@gmail.com>
2020-12-16 05:03:41 -0300

    googleMapsAPI added, MapUbication feature added

--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationDetailDialogFragment.kt
@@ -1,60 +1,68 @@
 package com.platzi.conf.view.ui.fragments
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.platzi.conf.R
+import com.platzi.conf.model.Ubication
+import kotlinx.android.synthetic.main.fragment_ubication_detail_dialog.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UbicationDetailDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UbicationDetailDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class UbicationDetailDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UbicationDetailDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UbicationDetailDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbarUbication.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarUbication.setTitleTextColor(Color.WHITE)
+        toolbarUbication.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val ubication = Ubication()
+
+        toolbarUbication.title = ubication.name
+
+        tvDetailNombreLugar.text = ubication.name
+        tvUbicationDireccion.text = ubication.address
+        tvUbicationTelefono.text = ubication.phone
+        tvUbicationSitioWeb.text = ubication.website
+
+        llTelefonoLugar.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${ubication.phone}")
             }
+            startActivity(intent)
+        }
+
+        llSitioWeb.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(ubication.website)
+            startActivity(intent)
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,60 +1,64 @@
 package com.platzi.conf.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 import com.platzi.conf.R
+import com.platzi.conf.model.Ubication
+import kotlinx.android.synthetic.main.fragment_ubication.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UbicationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UbicationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UbicationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UbicationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Load map on screen
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+
+        //zoom 1-19
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitud, ubication.latitud)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitud, ubication.longitud)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("PlatziConf")
+
+        //Load markerPlatziIcon
+        val bitMapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitMapDraw.bitmap, 150, 150, false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        googleMap?.addMarker(markerOptions)
+        googleMap?.setOnMarkerClickListener(this)
+
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.custom_map))
+
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailDialogFragment)
+        return  true
+    }
+
 }
