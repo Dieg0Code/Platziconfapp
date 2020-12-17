c915136eaf630117e74d5596183475845baacb80
Diego Obando <diegoobando20@gmail.com>
2020-12-17 03:24:09 -0300

    SplashScreen added, some interfaces details corrected

--- app/src/main/java/com/platzi/conf/view/ui/activities/SplashscreenActivity.kt
+++ app/src/main/java/com/platzi/conf/view/ui/activities/SplashscreenActivity.kt
@@ -1,12 +1,37 @@
 package com.platzi.conf.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.platzi.conf.R
+import kotlinx.android.synthetic.main.activity_splashscreen.*
 
 class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        ivLogoPlatziConf.startAnimation(animacion)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animacion.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()//this function destroy the activity
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+
     }
 }
